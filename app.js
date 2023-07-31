d53cd9fd3476fcacfaa3298b535a30073f2bbbb1
Shahriyor006 <abdujalilovshaxriyor3@gmail.com>
2023-07-31 15:52:56 +0500

    navbar bn sticky

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("js")
+
+// let ism = prompt("Your name")
+
+// console.log("Hello "+ Shakhriyor);
+
+// let yil = prompt("Tug`ilgan yiliz")
+
+// console.log(2023 - yil + " yosh");
+
+// console.log("Men " + ism + " " + (2023 - yil) + " yoshman");
+
+// console.log(`Men ${ism} ${2023 - yil} yoshman`);
+
+let a = prompt("a="),
+b=prompt("b=") 
+
+console.log(`${a} + ${b}=${+a + +b}`);
+
+
+
+
+
